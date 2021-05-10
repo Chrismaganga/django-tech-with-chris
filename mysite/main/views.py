6751ec478e38bb1043e9b032c5db68cdb26b02d7
NerdvanaNC <nickunjchopra@gmail.com>
2021-05-11 00:36:47 +0530

    Followed along with more of the tutorial; added models, migrated them, and populated database with a test entry; created views and set them up to take in arguments from the URL.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
+
+def index(response, id):
+  ls = ToDoList.objects.get(id=id)
+  item = ls.item_set.get(id=1)
+  
+  return HttpResponse(f"<h1>{ls.name}</h1><ul><li>{item.text}</li></ul>")
+
