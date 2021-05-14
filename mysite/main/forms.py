82f9c205a85eac88c472222e34cc806692a05265
NerdvanaNC <nickunjchopra@gmail.com>
2021-05-14 22:51:29 +0530

    Added Bootstrap/Styles; facelifted the entire project; looks much nicer.

--- mysite/main/forms.py
+++ mysite/main/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from django import forms
 
 class CreateNewList(forms.Form):
-  name = forms.CharField(label="List Name", max_length=200)
+  name = forms.CharField(label="List Name", max_length=200, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter to submit..."}))
