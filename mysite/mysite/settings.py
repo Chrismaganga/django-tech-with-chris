fc96eaacefad6e3c8f0af062caf525f7773df2db
NerdvanaNC <nickunjchopra@gmail.com>
2021-05-21 17:59:31 +0530

    Fixing settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,9 +14,7 @@ from pathlib import Path
 import os
 import environ
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = env('DEBUG')
+DEBUG = False if (env('DEBUG') == False) else True
 
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
