607584d0c5fde419ff6c8f2b6ae6fa35a3dadaf3
NerdvanaNC <nickunjchopra@gmail.com>
2021-05-21 18:02:17 +0530

    Fixing settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False if (env('DEBUG') == 'False') else True
+DEBUG = True
+# DEBUG = False
 
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
