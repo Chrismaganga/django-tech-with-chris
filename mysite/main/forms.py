c8e0bf600bb5f1562a975b58cacce35e29d80d33
chrismaganga <chrismaganga3@gmail.com>
2022-11-20 12:25:07 +0200

    update

--- mysite/main/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class CreateNewList(forms.Form):
-  name = forms.CharField(label="List Name", max_length=200, widget=(
-    forms.TextInput(attrs={"class": "form-control", "placeholder": "New List"})))
--- mysite/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path("view/<int:id>/", views.list_view, name="ListView"),
-  path("", views.view, name="View"),
-]
